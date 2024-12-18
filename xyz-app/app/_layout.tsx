b0e6f78790235299adf68b7c9bfad81fc611c962
bckyrd-io <bckyrd.io@gmail.com>
2024-12-18 15:12:07 +0000

    customizing drawer

--- xyz-app/app/_layout.tsx
+++ xyz-app/app/_layout.tsx
@@ -1,32 +1,51 @@
-// import { Stack } from "expo-router";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import CaptureScreen from "./capture";
+import AnalysisScreen from "./analyse";
+import UsersScreen from "./users";
 
-// export default function RootLayout() {
-//   return <Stack />;
-// }
+const Drawer = createDrawerNavigator();
 
-
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer';
-
-export default function Layout() {
+export default function DrawerLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: "#fff",
+            borderRadius: 0, // Removes any curves
+          },
+          drawerContentContainerStyle: {
+            borderRadius: 0, // Ensures no curves on the drawer content container
+          },
+          drawerItemStyle: {
+            borderRadius: 0,
+          },
+          drawerActiveBackgroundColor: "#eff", // Primary green for selected item
+          drawerActiveTintColor: "#10b981", // White text for selected item
+          drawerInactiveTintColor: "#000", // Black text for unselected items
+          drawerType: "front",
+          drawerContentStyle: {
+            borderRadius: 0,
+          }
+        }}
+      >
         <Drawer.Screen
-          name="index" // This is the name of the page and must match the url from root
-          options={{
-            drawerLabel: 'Home',
-            title: 'overview',
-          }}
+          name="Capture"
+          component={CaptureScreen}
+          options={{ drawerLabel: "Capture" }}
         />
         <Drawer.Screen
-          name="user/[id]" // This is the name of the page and must match the url from root
-          options={{
-            drawerLabel: 'User',
-            title: 'overview',
-          }}
+          name="Analysis"
+          component={AnalysisScreen}
+          options={{ drawerLabel: "Analysis" }}
         />
-      </Drawer>
+        <Drawer.Screen
+          name="Users"
+          component={UsersScreen}
+          options={{ drawerLabel: "Users" }}
+        />
+      </Drawer.Navigator>
     </GestureHandlerRootView>
   );
 }
